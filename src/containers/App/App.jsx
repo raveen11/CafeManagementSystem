0cbe7be86804e8c22688379188c67a0dd2a39470
raveen11 <rabin.neupane.treeleaf@gmail.com>
2025-01-06 23:16:50 +0545

    added tanstack query

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import MainRoutingComponent from "../../routes/MainRoutingComponent";
 import { AppLayoutWrapper } from "./style";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const App = () => {
+  // Create a client
+  const queryClient = new QueryClient();
   return (
-    <AppLayoutWrapper>
-      App
-      <MainRoutingComponent />
-    </AppLayoutWrapper>
+    // Provide the client to your App
+    <QueryClientProvider client={queryClient}>
+      <AppLayoutWrapper>
+        App
+        <MainRoutingComponent />
+      </AppLayoutWrapper>
+    </QueryClientProvider>
   );
 };
 
